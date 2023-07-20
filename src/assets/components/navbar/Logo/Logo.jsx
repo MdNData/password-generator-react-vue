7ae59bcd7a19d5dea50f105985edd6582187adad
Dra <nd.mirica@gmail.com>
2023-07-20 16:24:27 +0300

    up

--- src/assets/components/navbar/Logo/Logo.jsx
+++ src/assets/components/navbar/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import { dataLogo } from "../../../data/navbar/Logo/dataLogo";
 export const Logo = () => {
   return (
     <div className="nav-logo">
-      <a href="/index.html">
+      <a href="./index.html">
         <img src={dataLogo.logo} alt={dataLogo.logoDescription} />
         <h2>{dataLogo.name}</h2>
       </a>
