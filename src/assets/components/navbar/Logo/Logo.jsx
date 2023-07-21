9bcecfab1ac8b562a00a177dd4ea823a0784d42c
Dra <nd.mirica@gmail.com>
2023-07-21 09:50:43 +0300

    update

--- src/assets/components/navbar/Logo/Logo.jsx
+++ src/assets/components/navbar/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import { dataLogo } from "../../../data/navbar/Logo/dataLogo";
 export const Logo = () => {
   return (
     <div className="nav-logo">
-      <a href="#">
+      <a href="/password-generator-react-vue/">
         <img src={dataLogo.logo} alt={dataLogo.logoDescription} />
         <h2>{dataLogo.name}</h2>
       </a>
